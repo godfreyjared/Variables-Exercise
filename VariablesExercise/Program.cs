d3c012f2f9b377ff866275b63cb525fe2814054f
Jared Godfrey <godfreyjared@gmail.com>
2025-07-12 19:08:27 -0600

    Practice isfinished.

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userName = "Jared";
+            int age = 30;
+            double avg = 98.7;
+            decimal bankAccount = 100.00m;
+            char initial = 'J';
+            bool isMarried = true;
+
+            Console.WriteLine($"Welcome, {userName}. According to our records, you are {age}. The average of your test scores is {avg}. \nYour most recent gift deposit is {bankAccount}. Your initial being {initial}. And since you are married per our records. Answer: {isMarried}. Your spouse has access as well.");
         }
     }
 }
